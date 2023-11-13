3752fc85bd0ffb948758c56243f7b933966ce17c
albjonam <98861930+albjonam@users.noreply.github.com>
2023-11-13 16:04:28 -0500

    Add files via upload

---
+++ number_guessing_game.py
@@ -0,0 +1,40 @@
+# Generate a random number and track how many times it takes the user to guess this number 
+
+import random
+
+top_of_range = input("Type a number: ")
+
+if top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    
+    if top_of_range <= 0:
+         print("Type a number larger than 0 next time.")
+         quit()
+else:
+    print("Type a number to play the game.")   
+    quit()     
+      
+
+random_number = random.randint(0, top_of_range)
+guesses = 0
+
+while True:
+    guesses += 1
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time.")   
+        continue
+    
+    if user_guess == random_number:
+        print("You are correct!")
+        break
+    elif user_guess > random_number:
+            print("You are above the number.")
+    else:
+        print("You are below the number.")
+        
+
+print("You got it in", guesses, "guesses")
+  
